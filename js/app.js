b3b27d61464120b7bbf8a508a7dbab7d1d8fd632
Crystal Laing <cistle@gmail.com>
2021-06-15 15:11:55 -0700

    JS/Color Palette
    Added animation plugin (sal) and adjusted a few styles. Uploaded color palette for my own reference.

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// ES6 modules
+import sal from 'sal.js'
+
+// CommonJS modules
+var sal = require('sal.js')
+
+// Webpack
+@import '~sal.js/sal.css';
+
+// Other
+@import './node_modules/sal.js/dist/sal.css';
+
+sal();
