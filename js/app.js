703a3dce4b5871595d01711b2d822b4cdfe010ae
Crystal Laing <cistle@gmail.com>
2021-06-16 10:32:46 -0700

    Working animation
    Animation works (sal.js edit) , tried out another animation that didn't work so took that out. Added JS plan to app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
-// ES6 modules
-import sal from 'sal.js'
+/*==================================================================
+    JavaScript for Treehouse Project 9 Capstone Portfolio
 
-// CommonJS modules
-var sal = require('sal.js')
+    app.js
 
-// Webpack
-@import '~sal.js/sal.css';
+    1. Includes JS plugin for sliding content on screen, taken 
+    from: https://github.com/mciastek/sal
+    Execute in this file by running: sal();
+    and including sal.js, sal.css
 
-// Other
-@import './node_modules/sal.js/dist/sal.css';
+    2. Rotation/Animation for imaages at top of screen
 
+    3. Popup window for images at top of screen when clicked
+    
+    4. Popup window for portfolio cards when clicked
+
+==================================================================*/
+
+
+/* slide-up animation for each section */
 sal();
+
+
+
